db47ed40c90c55c51a5762d4fb15942a240c2014
Lucille81 <milakis2601@gmail.com>
2022-06-10 10:42:29 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let units = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+
+    let dozens = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    let mod = number % 10;
+
+    if (number < 20) return units[number];
+    if (number < 100)
+        return `${dozens[Math.trunc(number / 10) - 2]}${
+            mod ? " " + units[mod] : ""
+        }`;
+    if (number < 1000)
+        return `${units[Math.trunc(number / 100)]} hundred ${
+            number % 100 == 0 ? "" : toReadable(number % 100)
+        }`.trim();
+};
